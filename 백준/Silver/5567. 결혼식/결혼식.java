cf1ce943897b47ed1552d80fc94f5bb84633555c
say1-e <tpdnjs4421@gmail.com>
2025-09-14 22:27:53 +0900

    [Silver II] Title: 결혼식, Time: 164 ms, Memory: 18780 KB -BaekjoonHub

--- 백준/Silver/5567. 결혼식/결혼식.java
+++ 백준/Silver/5567. 결혼식/결혼식.java
@@ -2,7 +2,6 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
@@ -11,10 +10,9 @@ import java.util.StringTokenizer;
 public class Main {
 
 	public static void main(String[] args) throws NumberFormatException, IOException {
-		BufferedReader br=  new BufferedReader(new InputStreamReader(System.in));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		int M = Integer.parseInt(br.readLine());
-		
 		List<Integer>[] friends = new ArrayList[N+1];
 		for (int i = 0 ; i < N+1 ; i++) friends[i] = new ArrayList<>();
 		for (int i = 0 ; i < M ; i++) {
@@ -26,26 +24,23 @@ public class Main {
 		}
 		
 		int[] dist = new int[N+1];
-		Arrays.fill(dist, -1);
-		
+		boolean[] visited = new boolean[N+1];
 		Queue<Integer> q = new LinkedList<>();
-		q.add(1);
-		dist[1] = 0;
-		
 		int cnt = 0;
-		while (!q.isEmpty()) {
+		q.offer(1);
+		visited[1] = true;
+		while(!q.isEmpty()) {
 			int cur = q.poll();
-			if (dist[cur] == 2) continue;
-			
-			for (int next : friends[cur]) {
-				if (dist[next] != -1) continue;
-				dist[next] = dist[cur] +1 ;
-				q.add(next);
-				if (dist[next] >= 1 && dist[next] <= 2) cnt++;
+			if (dist[cur] >= 2) continue;
+			for (int i : friends[cur]) {
+				if (visited[i]) continue;
+				visited[i] = true;
+				dist[i] = dist[cur]+1;
+				q.offer(i);
+				if (dist[i] >= 1 || dist[i] <= 2) cnt++;
 			}
-			
 		}
-		System.out.println(cnt);
+		System.out.print(cnt);
 	}
 
 }
