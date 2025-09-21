0eb2b81ac4b1e00f6e39abae362500e7fca4b4f6
say1-e <tpdnjs4421@gmail.com>
2025-09-21 20:25:52 +0900

    [level 3] Title: 단어 변환, Time: 0.51 ms, Memory: 85 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -0,0 +1,44 @@
+import java.util.*;
+class Solution {
+    static boolean[] visited;
+    static int minCnt;
+    static String target;
+    static String[] words;
+    public int solution(String begin, String target, String[] words) {
+        boolean none = false;
+        for (String word : words){
+            if (target.equals(word)) none = true;
+        }
+        if (!none) return 0;
+        
+        this.minCnt = Integer.MAX_VALUE;
+        this.target = target;
+        this.words = words;
+        this.visited = new boolean[words.length];
+        dfs(begin, 0);
+        return minCnt == Integer.MAX_VALUE ? 0 : minCnt;
+    }
+    
+    public static void dfs(String str, int cnt){
+        if (cnt >= minCnt) return;
+        if (str.equals(target)){
+            minCnt = Math.min(minCnt, cnt);
+            return;
+        }
+        
+        for (int i = 0 ; i < words.length ; i++){
+            if (visited[i]) continue;
+            int diff = 0;
+            for (int j = 0 ; j < str.length() ; j++){
+                if (str.charAt(j) != words[i].charAt(j)){
+                    diff++;
+                }
+            }
+            if (diff > 1) continue;
+            visited[i] = true;
+            dfs(words[i], cnt + 1);
+            visited[i] = false;
+        }
+        return;
+    }
+}
