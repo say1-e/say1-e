dca229524c2219f73e3a3f39a343fed136bdc610
Sewon Lee <tpdnjs4421@gmail.com>
2026-01-03 23:14:15 +0900

    [Silver IV] Title: 비밀번호 찾기, Time: 564 ms, Memory: 64688 KB -BaekjoonHub

---
+++ 백준/Silver/17219. 비밀번호 찾기/비밀번호 찾기.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine()," ");
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		Map <String, String> map = new HashMap<>();
+		for (int n = 0 ; n < N ; n++) {
+			st = new StringTokenizer(br.readLine(), " ");
+			map.put(st.nextToken(), st.nextToken());
+		}
+		StringBuffer sb = new StringBuffer();
+		for (int m = 0 ; m < M ; m++) {
+			sb.append(map.get(br.readLine())+"\n");
+		}
+		System.out.println(sb.toString().trim());
+	}
+}
